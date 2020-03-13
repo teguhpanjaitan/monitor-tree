0200beb72d669ad2ae73c681b70e9f7bd2c41c7c
Teguh <teguhpanjaitan@gmail.com>
2020-03-13 22:44:53 +0700

    Fix breadcrumb

--- application/views/theme/notebook/page/bulk_import.php
+++ application/views/theme/notebook/page/bulk_import.php
@@ -1,6 +1,6 @@
 <ul class="breadcrumb no-border no-radius b-b b-light pull-in">
 	<li><a href="/"><i class="fa fa-home"></i> Home</a></li>
-	<li><a href="#">Pohon</a></li>
+	<li><a href="#">Bulk Import</a></li>
 </ul>
 <div class="m-b-md">
 	<h3 class="m-b-none">Bulk Import</h3>
@@ -14,9 +14,4 @@
 
 <?php 
 global $template; 
-$template->footer_add = "
-<script src='".base_url("assets/notebook/")."/js/datatables/jquery.dataTables.min.js'></script>
-
-<script>
-</script>
-";
+$template->footer_add = "";
--- application/views/theme/notebook/page/jenis_pohon.php
+++ application/views/theme/notebook/page/jenis_pohon.php
@@ -1,6 +1,6 @@
 <ul class="breadcrumb no-border no-radius b-b b-light pull-in">
 	<li><a href="/"><i class="fa fa-home"></i> Home</a></li>
-	<li><a href="#">Pohon</a></li>
+	<li><a href="#">Jenis Pohon</a></li>
 </ul>
 <div class="m-b-md">
 	<h3 class="m-b-none">Jenis Pohon</h3>
