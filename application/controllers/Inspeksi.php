c470dd10215578b7cc30d73278cb168a43bd4e4b
Teguh <teguhpanjaitan@gmail.com>
2020-07-02 21:04:42 +0700

    Rename point become inspeksi

--- application/controllers/Inspeksi.php
+++ application/controllers/Inspeksi.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-class Point extends CI_Controller
+class Inspeksi extends CI_Controller
 {
 
 	function __construct()
--- application/views/theme/notebook/sidebar.php
+++ application/views/theme/notebook/sidebar.php
@@ -1,29 +1,34 @@
 <!-- .aside -->
-<aside class="bg-dark lter aside-md hidden-print hidden-xs" id="nav">          
+<aside class="bg-dark lter aside-md hidden-print hidden-xs" id="nav">
 	<section class="vbox">
 		<section class="w-f scrollable">
 			<div class="slim-scroll" data-height="auto" data-disable-fade-out="true" data-distance="0" data-size="5px" data-color="#333333">
-                
+
 				<!-- nav -->
 				<nav class="nav-primary hidden-xs">
 					<ul class="nav">
-						<li <?php if($_SERVER[REQUEST_URI] == "/") echo "class='active'"?>>
-							<a href="/"  >
+						<li <?php if ($_SERVER[REQUEST_URI] == "/") echo "class='active'" ?>>
+							<a href="/">
 								<i class="fa fa-dashboard icon">
 									<b class="bg-warning"></b>
 								</i>
 								<span>Dashboard</span>
 							</a>
 						</li>
-						<li <?php if(strpos($_SERVER[REQUEST_URI],'jenis_pohon') !== false || strpos($_SERVER[REQUEST_URI],'point') !== false || strpos($_SERVER[REQUEST_URI],'bulk_import') !== false) echo "class='active'"?>>
-							<a href="#"  >
+						<li <?php if (
+								strpos($_SERVER[REQUEST_URI], 'jenis_pohon') !== false
+								|| strpos($_SERVER[REQUEST_URI], 'inspeksi') !== false 
+								|| strpos($_SERVER[REQUEST_URI], 'eksekusi') !== false 
+								|| strpos($_SERVER[REQUEST_URI], 'bulk_import') !== false
+							) echo "class='active'" ?>>
+							<a href="#">
 								<i class="fa fa-table icon">
 									<b class="bg-success"></b>
 								</i>
-								<span>Pohon</span>
+								<span>Manajemen</span>
 							</a>
 							<ul class="nav lt">
-								<li <?php if(strpos($_SERVER[REQUEST_URI],'jenis_pohon') !== false) echo "class='active'"?>>
+								<li <?php if (strpos($_SERVER[REQUEST_URI], 'jenis_pohon') !== false) echo "class='active'" ?>>
 									<a href="/jenis_pohon">
 										<i class="fa fa-angle-right"></i>
 										<span>Jenis Pohon</span>
@@ -31,15 +36,23 @@
 								</li>
 							</ul>
 							<ul class="nav lt">
-								<li <?php if(strpos($_SERVER[REQUEST_URI],'point') !== false) echo "class='active'"?>>
-									<a href="point">
+								<li <?php if (strpos($_SERVER[REQUEST_URI], 'inspeksi') !== false) echo "class='active'" ?>>
+									<a href="inspeksi">
 										<i class="fa fa-angle-right"></i>
-										<span>Pohon</span>
+										<span>Inspeksi</span>
 									</a>
 								</li>
 							</ul>
 							<ul class="nav lt">
-								<li <?php if(strpos($_SERVER[REQUEST_URI],'bulk_import') !== false) echo "class='active'"?>>
+								<li <?php if (strpos($_SERVER[REQUEST_URI], 'eksekusi') !== false) echo "class='active'" ?>>
+									<a href="eksekusi">
+										<i class="fa fa-angle-right"></i>
+										<span>Eksekusi</span>
+									</a>
+								</li>
+							</ul>
+							<ul class="nav lt">
+								<li <?php if (strpos($_SERVER[REQUEST_URI], 'bulk_import') !== false) echo "class='active'" ?>>
 									<a href="/bulk_import">
 										<i class="fa fa-angle-right"></i>
 										<span>Bulk Import</span>
@@ -47,33 +60,33 @@
 								</li>
 							</ul>
 						</li>
-						<li <?php if(strpos($_SERVER[REQUEST_URI],'map') !== false) echo "class='active'"?>>
-							<a href="/map"  >
+						<li <?php if (strpos($_SERVER[REQUEST_URI], 'map') !== false) echo "class='active'" ?>>
+							<a href="/map">
 								<i class="fa fa-map-marker icon">
 									<b class="bg-primary dker"></b>
 								</i>
 								<span>Monitoring</span>
 							</a>
 						</li>
-						<li <?php if(strpos($_SERVER[REQUEST_URI],'user') !== false) echo "class='active'"?>>
-							<a href="/user"  >
+						<li <?php if (strpos($_SERVER[REQUEST_URI], 'user') !== false) echo "class='active'" ?>>
+							<a href="/user">
 								<i class="fa fa-user icon">
 									<b class="bg-danger"></b>
 								</i>
 								<span>Kredensial</span>
 							</a>
-						</li>              
+						</li>
 					</ul>
-                </nav>
-                <!-- / nav -->
-				
+				</nav>
+				<!-- / nav -->
+
 			</div>
 		</section>
-            
+
 		<footer class="footer lt hidden-xs b-t b-dark">
 			<a href="#nav" data-toggle="class:nav-xs" class="pull-right btn btn-sm btn-dark btn-icon">
 				<i class="fa fa-angle-left text"></i>
-                <i class="fa fa-angle-right text-active"></i>
+				<i class="fa fa-angle-right text-active"></i>
 			</a>
 		</footer>
 	</section>
