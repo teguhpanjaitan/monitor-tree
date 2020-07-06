0e5760b21bfd8061376b1cee348bf33416b072e9
Teguh <teguhpanjaitan@gmail.com>
2020-07-06 19:26:21 +0700

    Fix error

--- application/models/ajax/Get_all_tree.php
+++ application/models/ajax/Get_all_tree.php
@@ -6,7 +6,7 @@ class Get_all_tree extends CI_Model
 	public function exec()
 	{
 		$this->db->select("jp.name as jenis_pohon,p.*")
-			->from("inspeksi as p")
+			->from("pohon as p")
 			->join("jenis_pohon as jp", "jp.id = p.id_jenis_pohon", "left")
 			->where("p.deleted", "0");
 		$ret = $this->db->get()->result_array();
@@ -19,7 +19,7 @@ class Get_all_tree extends CI_Model
 			$temp['jenis_pohon'] = $val['jenis_pohon'];
 			$temp['tinggi'] = $val['tinggi'];
 			$temp['segmen'] = $val['segmen'];
-			$temp['image'] = $val['image'];
+			$temp['image'] = empty($val['image']) ? '' : $val['image'];
 			$ret[$key] = $temp;
 		}
 		return $ret;
